3abcdebf07ea637b0f38073d706e5c28a677023a
Matsura Yuma <lemonburst1958@gmail.com>
2022-12-21 16:18:21 +0900

    Create cli

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import fs from "fs/promises";
+import path from "path";
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import { generateCss } from "@camome/system";
+
+yargs(hideBin(process.argv))
+  .command(
+    "generate-css",
+    "Generate CSS",
+    (yargs) =>
+      yargs.options({
+        config: {
+          type: "string",
+          default: "camome.config.js",
+        },
+        output: {
+          type: "string",
+          default: "camome.css",
+        },
+      }),
+    async (options) => {
+      // TODO: validate config with zod or something.
+      const config = (await import(path.join(process.cwd(), options.config)))
+        .default;
+      const css = await generateCss(config.theme);
+      const outputPath = path.join(process.cwd(), options.output);
+      await fs.writeFile(outputPath, css);
+      console.log(`CSS file exported to:\n${outputPath}`);
+    }
+  )
+  .strict()
+  .alias({ h: "help" }).argv;
---
+++ packages/cli/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["./src/index.ts", "./src/cli/index.ts"],
+  clean: true,
+  format: ["esm"],
+  dts: true,
+  outExtension(ctx) {
+    return { js: `.${ctx.format}.js` };
+  },
+});
