717e8288eeda451763e0140f0519e71d94548afe
Matsura Yuma <lemonburst1958@gmail.com>
2023-01-06 13:27:41 +0900

    Remove unused packages

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { getSandpackCssText } from "@codesandbox/sandpack-react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -9,10 +8,6 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <style
-          dangerouslySetInnerHTML={{ __html: getSandpackCssText() }}
-          id="sandpack"
-        />
       </Head>
       <body>
         <Main />
