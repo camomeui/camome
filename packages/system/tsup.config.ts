e90dbab504c36230c31708ba8d944502ad17864f
Matsura Yuma <lemonburst1958@gmail.com>
2023-01-11 19:03:39 +0900

    Update

--- packages/system/tsup.config.ts
+++ packages/system/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["./src/index.ts", "./src/cli/index.ts"],
-  clean: true,
+  clean: false,
   format: ["esm", "cjs"],
   dts: true,
   minify: true,
--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  clean: true,
+  clean: false,
   dts: true,
   format: ["esm", "cjs"],
   outExtension(ctx) {
