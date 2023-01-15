47a2119a3068c5724418688e465aa022b8bd4cb9
Matsura Yuma <lemonburst1958@gmail.com>
2023-01-15 16:40:57 +0900

    Modify Menu

--- packages/core/src/components/Menu/variables.ts
+++ packages/core/src/components/Menu/variables.ts
@@ -3,7 +3,7 @@ import { cssVar } from "@camome/system";
 import { ComponentCssVariables } from "../../types";
 
 const variables = {
-  iconSize: "1.25rem",
+  iconSize: "1em",
   iconColor: cssVar("color.font.subtle"),
   fontColor: cssVar("color.font.base"),
 } as const satisfies ComponentCssVariables;
