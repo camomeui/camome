97161472ed225de172d237074ef436d84a93ae6b
Matsura Yuma <lemonburst1958@gmail.com>
2023-01-21 13:27:37 +0900

    Update

--- scripts/test-storybook.sh
+++ scripts/test-storybook.sh
@@ -2,5 +2,5 @@
 
 cd apps/storybook
 pnpm concurrently -k -s first -n "SB,TEST" -c "magenta,blue" \
-  "pnpm http-server storybook-static --port 6006 --silent" \
+  "pnpm http-server storybook-static --port 6006" \
   "pnpm -v wait-on tcp:6006 && pnpm -F storybook run test --url http://127.0.0.1:6006"
