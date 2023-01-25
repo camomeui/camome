b78789197c5d6672719adccf63bd15aa1a1046a0
Matsura Yuma <lemonburst1958@gmail.com>
2023-01-26 02:35:16 +0900

    Update

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,8 +1,14 @@
 import fs from "fs/promises";
 import path from "path";
 
+import esbuild, { OnLoadResult } from "esbuild";
+import { sassPlugin } from "esbuild-sass-plugin";
 import { globby } from "globby";
+import postcss from "postcss";
+import postcssModulesPlugin from "postcss-modules";
 import { defineConfig } from "tsup";
+
+import { buildScopedClassName } from "@camome/utils";
 
 async function copyScssFiles(src: string, dist: string) {
   const files = await globby(`${src}/**/*.scss`);
@@ -25,25 +31,83 @@ async function fixScssImports(dir: string) {
   }
 }
 
+async function createBulletFile(dist: string) {
+  const componentDirs = await globby([`${dist}/*`, "!*/styles"], {
+    onlyDirectories: true,
+  });
+  let esmContent = "";
+  let dtsContent = "";
+  let cjsRequires = "";
+  let cjsModules = "";
+  for (const dirPath of componentDirs) {
+    const componentName = path.basename(dirPath);
+    esmContent += `export * from "./${componentName}/index.mjs";\n`;
+    dtsContent += `export * from "./${componentName}";\n`;
+    cjsRequires += `const ${componentName} = require("./${componentName}/index.cjs");\n`;
+    cjsModules += `  ...${componentName},\n`;
+  }
+  const cjsExports = `module.exports = {\n${cjsModules}\n}`;
+  const cjsContent = `${cjsRequires}\n\n${cjsExports}`;
+  await fs.writeFile(path.join(dist, "index.mjs"), esmContent);
+  await fs.writeFile(path.join(dist, "index.d.ts"), dtsContent);
+  await fs.writeFile(path.join(dist, "index.cjs"), cjsContent);
+}
+
+async function generateBundledCss(dist: string) {
+  await esbuild.build({
+    entryPoints: [path.join(dist, "index.mjs")],
+    outdir: dist,
+    plugins: [
+      sassPlugin({
+        transform: postcssModules(),
+      }),
+    ],
+    bundle: true,
+  });
+  await Promise.all([
+    fs.rm(path.join(dist, "index.js")),
+    fs.rename(path.join(dist, "index.css"), path.join(dist, "components.css")),
+  ]);
+}
+
+export function postcssModules() {
+  return async function (source: string, path: string): Promise<OnLoadResult> {
+    const { css } = await postcss([
+      postcssModulesPlugin({
+        generateScopedName(name, filename) {
+          return buildScopedClassName(name, filename);
+        },
+        getJSON() {
+          return;
+        },
+      }),
+    ]).process(source, { from: path, map: false });
+
+    return {
+      contents: css,
+      loader: "css",
+    };
+  };
+}
+
 export default defineConfig(async () => {
   const entries = await globby("./src/components/*/index.tsx");
   return {
     entry: entries,
-    clean: true,
-    format: ["esm", "cjs"],
-    dts: true,
-    minify: false,
+    minify: true,
     sourcemap: true,
-    loader: {
-      ".module.scss": "copy",
+    treeshake: true,
+    dts: true,
+    format: ["esm", "cjs"],
+    outExtension(ctx) {
+      return { js: ctx.format === "esm" ? ".mjs" : ".cjs" };
     },
     esbuildOptions: (options) => {
       options.assetNames = "[dir]/[name]";
-      options.splitting = false;
       return options;
     },
-    outExtension(ctx) {
-      return { js: ctx.format === "esm" ? ".mjs" : ".cjs" };
+    loader: {
+      ".scss": "copy",
     },
     onSuccess: async () => {
       await Promise.all([
@@ -51,6 +115,8 @@ export default defineConfig(async () => {
         copyScssFiles("./src/styles", "./dist/styles"),
       ]);
       await fixScssImports("./dist");
+      await createBulletFile("./dist");
+      await generateBundledCss("./dist");
     },
   };
 });
