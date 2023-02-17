6b1c85cd898211a4821db23f87b63f23241c1c16
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-17 12:05:16 +0900

    Enable web font

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400..800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
